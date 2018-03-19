821a24aec3329c9136974a916582ea1dde9e0442
deva jayantha <devajayantha870@gmail.com>
2018-03-20 00:27:09 +0800

    first comit

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.BtnBrowse = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.BtnGrayscale = New System.Windows.Forms.Button()
+        Me.BtnBiner = New System.Windows.Forms.Button()
+        Me.BtnBrightnes = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtnClose = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 74)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(244, 298)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.PictureBox2.Location = New System.Drawing.Point(266, 75)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(244, 298)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.PictureBox3.Location = New System.Drawing.Point(521, 74)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(244, 298)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'BtnBrowse
+        '
+        Me.BtnBrowse.Font = New System.Drawing.Font("Lucida Console", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnBrowse.Location = New System.Drawing.Point(12, 391)
+        Me.BtnBrowse.Name = "BtnBrowse"
+        Me.BtnBrowse.Size = New System.Drawing.Size(70, 33)
+        Me.BtnBrowse.TabIndex = 3
+        Me.BtnBrowse.Text = "Browse"
+        Me.BtnBrowse.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'BtnGrayscale
+        '
+        Me.BtnGrayscale.Font = New System.Drawing.Font("Lucida Sans Unicode", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnGrayscale.Location = New System.Drawing.Point(453, 392)
+        Me.BtnGrayscale.Name = "BtnGrayscale"
+        Me.BtnGrayscale.Size = New System.Drawing.Size(90, 29)
+        Me.BtnGrayscale.TabIndex = 4
+        Me.BtnGrayscale.Text = "Grayscale"
+        Me.BtnGrayscale.UseVisualStyleBackColor = True
+        '
+        'BtnBiner
+        '
+        Me.BtnBiner.Font = New System.Drawing.Font("Lucida Sans Unicode", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnBiner.Location = New System.Drawing.Point(552, 392)
+        Me.BtnBiner.Name = "BtnBiner"
+        Me.BtnBiner.Size = New System.Drawing.Size(92, 29)
+        Me.BtnBiner.TabIndex = 5
+        Me.BtnBiner.Text = "Biner"
+        Me.BtnBiner.UseVisualStyleBackColor = True
+        '
+        'BtnBrightnes
+        '
+        Me.BtnBrightnes.Font = New System.Drawing.Font("Lucida Sans Unicode", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnBrightnes.Location = New System.Drawing.Point(652, 391)
+        Me.BtnBrightnes.Name = "BtnBrightnes"
+        Me.BtnBrightnes.Size = New System.Drawing.Size(112, 28)
+        Me.BtnBrightnes.TabIndex = 6
+        Me.BtnBrightnes.Text = "Brightness"
+        Me.BtnBrightnes.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Caviar Dreams", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(233, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(324, 27)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "PENGOLAHAN CITRA DIGITAL"
+        '
+        'BtnClose
+        '
+        Me.BtnClose.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnClose.Location = New System.Drawing.Point(737, 12)
+        Me.BtnClose.Name = "BtnClose"
+        Me.BtnClose.Size = New System.Drawing.Size(28, 27)
+        Me.BtnClose.TabIndex = 8
+        Me.BtnClose.Text = "X"
+        Me.BtnClose.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Caviar Dreams", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(338, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(114, 21)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "KELOMPOK 3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.ClientSize = New System.Drawing.Size(776, 431)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.BtnClose)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnBrightnes)
+        Me.Controls.Add(Me.BtnBiner)
+        Me.Controls.Add(Me.BtnGrayscale)
+        Me.Controls.Add(Me.BtnBrowse)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents BtnBrowse As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents BtnGrayscale As System.Windows.Forms.Button
+    Friend WithEvents BtnBiner As System.Windows.Forms.Button
+    Friend WithEvents BtnBrightnes As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents BtnClose As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+
+    Private Sub BtnBrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnBrowse.Click
+        Dim ofdlg As OpenFileDialog = New OpenFileDialog()
+        ofdlg.Filter = "Picture Files(*.jpg;*.bmp)|*.jpg;*.bmp"
+        ofdlg.FilterIndex = 1
+        If ofdlg.ShowDialog() = DialogResult.OK Then
+            PictureBox1.ImageLocation = ofdlg.FileName
+
+        End If
+    End Sub
+
+    Private Sub BtnGrayscale_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnGrayscale.Click
+        Dim gy As New Bitmap(PictureBox1.Image)
+        Dim x As Integer
+        Dim y As Integer
+        Dim clr As Integer
+
+        For x = 0 To gy.Width - 1
+            For y = 0 To gy.Height - 1
+                clr = (CInt(gy.GetPixel(x, y).R) + _
+                    gy.GetPixel(x, y).G + _
+                    gy.GetPixel(x, y).B) \ 3
+                gy.SetPixel(x, y, Color.FromArgb(clr, clr, clr))
+            Next
+        Next
+        PictureBox2.Image = gy
+
+    End Sub
+
+    Private Sub BtnBiner_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnBiner.Click
+        Dim bnr As New Bitmap(PictureBox1.Image)
+        Dim x As Integer
+        Dim y As Integer
+        Dim r, g, b, total As Integer
+        Dim col As Color
+
+
+        For x = 1 To bnr.Width - 1
+            For y = 1 To bnr.Height - 1
+                col = bnr.GetPixel(x, y)
+                r = col.R
+                g = col.G
+                b = col.B
+                total = (r + g + b) / 3
+                If total > 128 Then
+                    bnr.SetPixel(x, y, Color.White)
+                Else
+                    bnr.SetPixel(x, y, Color.Black)
+                End If
+
+            Next y
+        Next x
+        PictureBox3.Image = bnr
+    End Sub
+
+    Private Sub BtnClose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnClose.Click
+        Me.Close()
+    End Sub
+
+End Class
